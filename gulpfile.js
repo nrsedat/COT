d989539aad5e0c699da18265b7f8c641dd477ba3
Ratheesh <nrsnaithalath@gmail.com>
2015-12-08 00:35:49 -0500

    version 2 - styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,25 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var compass = require('gulp-compass');
 var minifyCSS = require('gulp-minify-css');
 
 var paths = {
-  sass : ['./sass/*.scss', './sass/**/*.scss']
+  sass: ['./app/sass/*.scss', './app/sass/**/*.scss']
 };
 
-
-gulp.task('compass', function() {
+gulp.task('compass', function () {
   gulp.src(paths.sass)
     .pipe(compass({
       css: 'app/assets/css',
-      sass: 'app/assets/sass',
+      sass: 'app/sass',
       image: 'app/assets/images'
     }))
     .pipe(minifyCSS())
     .pipe(gulp.dest('app/assets/temp'));
 });
 
-gulp.task('sass:watch', function(){
+gulp.task('sass:watch', function (){
   gulp.watch(paths.sass, ['compass']);
 });
 
